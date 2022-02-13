e54a0cf9c25a2b960fb99b43c4b54d97edbbc336
McWilton <mcwilton85@gmail.com>
2022-02-13 22:52:18 +0200

    admin update
    admin update

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 from .models import Profile
 
-admin.site.register(Profile)
+# admin.site.register(Profile)
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'student_id', 'phone', 'birthday', 'city')
